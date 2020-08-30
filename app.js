630f88a82ab9d76310cb3eb815943990f215df81
pradipb123 <pradeepbohora26@gmail.com>
2020-08-30 22:54:39 +0300

    add procfile for heroku deployment

--- app.js
+++ app.js
@@ -78,4 +78,6 @@ app.use(bodyParser.json());
 
 app.use("/api/packages", packageRouter);
 
-app.listen(3001, () => console.log("server running on port 3001"));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
