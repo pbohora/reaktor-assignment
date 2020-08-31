ff81dae111d557208417e53ea33fe9c7b0902adc
pradipb123 <pradeepbohora26@gmail.com>
2020-08-31 14:58:36 +0300

    add build

---
+++ client/build/precache-manifest.998fe970ae722fae8d6dddb922cf7168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42a96dc38e96335f578767db9de5ee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ca1c2bf5bf3eb9cb452",
+    "url": "/static/css/main.afe7424d.chunk.css"
+  },
+  {
+    "revision": "6e04d81db80ca4da32ae",
+    "url": "/static/js/2.7fbe2aaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7fbe2aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca1c2bf5bf3eb9cb452",
+    "url": "/static/js/main.ef5414c7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
